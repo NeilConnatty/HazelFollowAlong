17ae4a56d78c1edcad457a3241f205f3668a1651
Neil Connatty <neil.connatty@gmail.com>
2020-10-06 20:49:18 -0700

    flesh out logging interface

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -1,5 +1,7 @@
 #include "Log.h"
 
+#include "spdlog/sinks/stdout_color_sinks.h"
+
 namespace Hazel
 {
 
@@ -9,7 +11,12 @@ std::shared_ptr<spdlog::logger> Log::s_clientLogger;
 
 void Log::Init()
 {
-  
+  spdlog::set_pattern("%^[%T] %n: %v%$");
+  s_coreLogger = spdlog::stdout_color_mt("HAZEL");
+  s_coreLogger->set_level(spdlog::level::trace);
+
+  s_clientLogger = spdlog::stdout_color_mt("APP");
+  s_clientLogger->set_level(spdlog::level::trace);
 }
 
 }
